6c66eff8d598825d18d4e2f7ede0170ed6df967c
Ratul726 <ratulx726492@gmail.com>
2024-02-23 23:54:38 -0800

    first commit

---
+++ defualt.js
@@ -0,0 +1,31 @@
+function add(num1,last){
+    const result = num1 + last;
+    console.log(num1,last,result)
+    return result;
+}
+
+const first = 'jaan';
+const last = 'pakhi'
+
+// 
+
+const a = 10;
+const b = 20;
+const result = 'the sum of number is ' + a + ' and '+ b + ' is ' + (a + b) 
+// console.log(result);
+
+const numbers = [10,20,3,0,4];
+const students= {
+    name:'ratul',
+    age : 17
+}
+const math = `The sum of number is ${numbers[1]} and ${students.age} is ${numbers[1] + students.age}`
+console.log(math)
+
+
+const email = `My name is ratul
+javascript output can show 
+this code can you use the 
+console.log(email)`
+
+// console.log(email)
---
+++ index.js
@@ -0,0 +1,12 @@
+const numbers = [10,52,65,58,42];
+// numbers = [10,5,8,9,8]
+numbers[2] = 78;
+numbers.push(41,85,6,1)
+console.log(numbers)
+
+let sum = 0;
+for(let i = 0; i < 11; i++){
+    sum += i;
+    
+}
+console.log(sum)
